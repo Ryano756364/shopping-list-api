67d52f25491695d208055d7a11be4d29227df93a
rodell <ryanodell2668@gmail.com>
2023-02-12 13:46:57 -0500

    Item and review class setup with instances, get, set, and cons

---
+++ src/main/java/com/ryanpodell/shoppinglistapi/Item.java
@@ -0,0 +1,32 @@
+package com.ryanpodell.shoppinglistapi;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "items")
+@Data //takes care of getters, setters, and toString methods
+@AllArgsConstructor //takes care of constructor that takes all fields
+@NoArgsConstructor //takes care of no param constructor
+public class Item {
+
+    @Id // lets db know that the 'id' is unique identifier in db
+    private ObjectId id;
+    private String StringId;
+    private int sku;
+    private String name;
+    private List<String> brand;
+    private double price;
+    private String picture;
+    private List<String> category;
+    @DocumentReference  //stores only id of reviews and will be in separate collection -> manual ref relationship
+    private List<Review> reviewIds;  //embedded relationship with one to many setup in db
+    private String description;
+    private double weightInGrams;
+}
