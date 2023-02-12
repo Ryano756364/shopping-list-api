1cec7f52ba29fab9f51e4702f28608e4fd4594f3
Ryan O'Dell <110799654+Ryano756364@users.noreply.github.com>
2023-02-12 11:27:29 -0500

    Initial commit with SpringBoot

---
+++ shopping-list-api/src/test/java/com/ryanpodell/shoppinglistapi/ShoppingListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryanpodell.shoppinglistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
